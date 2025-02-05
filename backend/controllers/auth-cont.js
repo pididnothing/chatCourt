f7c2adc49cf268608f08270120038b1192a202c9
pididnothing <pranav24921@gmail.com>
2025-02-05 11:44:27 +0530

    refactored jwt and added login logout routes

--- backend/controllers/auth-cont.js
+++ backend/controllers/auth-cont.js
@@ -1,11 +1,50 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const login = (req, res) => {
-    res.send("Login User");
+import genTokenandCookie from "../utils/tokenGen.js";
+
+
+export const login = async (req, res) => {
+    try {
+        const {email, username, password} = req.body;
+        if(!email && !username){
+            return res.status(400).json({error: "Email or Username is required"});
+        }
+        if(!password){
+            return res.status(400).json({error: "Password is required"});
+        }
+        const user = await (email ? User.findOne({email: email}) : User.findOne({username: username}));
+        if(!user){
+            return res.status(400).json({error: "User not found"});
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if(!isMatch){
+            return res.status(400).json({error: "Invalid Password", password: password, userPassword: user.password});
+        }
+        genTokenandCookie(user._id, res);
+        res.status(200).json({
+            message: "User logged in successfully",
+            _id: user._id,
+            fullname: user.fullname,
+            username: user.username,
+            email: user.email,
+            role: user.role
+        });
+
+    }catch(error){
+        console.log("Error in login controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("Logout User");
+    try{
+        res.clearCookie('token');
+        res.status(200).json({message: "User logged out successfully"});
+    }catch(error){
+        console.log("Error in logout controller", error.message);
+        res.status(500).json({error: "Internal Server Error"});
+    }
 }
 
 export const signup = async (req, res) => {
@@ -33,9 +72,12 @@ export const signup = async (req, res) => {
             role
         });
 
+        genTokenandCookie(newUser._id, res);
+
         await newUser.save();
         res.status(200).json({
             message: "User created successfully",
+            _id: newUser._id,
             fullname: newUser.fullname,
             username: newUser.username,
             email: newUser.email,
--- backend/utils/tokenGen.js
+++ backend/utils/tokenGen.js
@@ -1,15 +1,18 @@
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-const tokenGen = (id, res) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: '30d',
-  });
+const genTokenandCookie = (id, res) => {
+  const token = jwt.sign({id}, process.env.JWT_SECRET, {
+      expiresIn: '30d'
+    });
 
   res.cookie('token', token, {
-    maxAge: 30*24*60*60*1000, // 30 days
-    httpOnly: true, // cookie cannot be accessed by client side js
-    sameSite: 'strict' // cookie can only be sent in same site
-  });
+      maxAge: 30*24*60*60*1000, // 30 days
+      httpOnly: true, // cookie cannot be accessed by client side js
+      sameSite: 'strict', // cookie can only be sent in same site
+      secure: process.env.NODE_ENV === 'production' // cookie can only be sent in https
+    });
+  
 }
 
-export default tokenGen;
+export default genTokenandCookie;
