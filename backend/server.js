54cba7785a3f4073d97192b474947266ae391014
pididnothing <pranav24921@gmail.com>
2025-02-25 17:17:11 +0530

    First Build

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import connectToMongo from "./db/connectToMongo.js";
+import path from "path";
 
 import authRoutes from "./routes/auth-routes.js";
 import courtRoutes from "./routes/court-routes.js";
@@ -12,20 +13,24 @@ import {app, server} from "./socket/socket.js";
  
 const PORT = process.env.PORT || 8000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // for parsing application/json
 app.use(cookieParser()); // for parsing cookies
-
-app.get("/",(req,res) => {      // home route
-    res.send("Hello World!!");
-});
 
 app.use("/api/auth",authRoutes); // all auth routes
 
 app.use("/api/court",courtRoutes); // all court routes
 
 app.use("/api/user",userRoutes); // all user routes
+
+app.use(express.static(path.join(__dirname, "..", "frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT,()=>{ 
     connectToMongo();
--- frontend/src/pages/landing/Landing.jsx
+++ frontend/src/pages/landing/Landing.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
     return (
-        <div>
-            <div>Loading</div>
+        <div className='h-screen flex flex-col justify-center items-center w-xs sm:w-xl md:w-3xl gap-2'>
+            <div className="w-full">
+                <div className="navbar bg-base-100 shadow-lg w-full rounded-box">
+                    <div className="flex-1">
+                        <p className="text-2xl font-bold font-mono">chatCourt</p>
+                    </div>
+                    <div className="flex-none">
+                        <Link to="/login">
+                            <button className="text-lg btn btn-ghost btn-square w-max font-mono p-1.5">Login</button>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className='h-max w-full bg-base-100 rounded-box font-mono text-center shadow-lg p-1.5'>
+                <span className='text-2xl font-extrabold'>About us</span>
+                <p className='text-justify font-bold'>Welcome to our chat-based debating platform, where structured arguments meet interactive discussions. Our platform is designed to simulate court proceedings, allowing users to engage in debates with a structured flow of arguments, witness and evidence presentation, and jury voting. Whether you're looking to refine your debating skills, engage in thought-provoking discussions, or experience a courtroom-like environment, our platform provides the tools to make it happen. With access control mechanisms, fair verdict presentations, and an immersive debate experience, we aim to revolutionize the way discussions unfold online. Stay tuned as we continue developing and enhancing our platform!</p>
+            </div>
         </div>
     )
 }
