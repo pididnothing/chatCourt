d5eb75ea36fba3fe9ec290fe54704cc1270d7c9e
pididnothing <pranav24921@gmail.com>
2025-06-03 11:31:36 +0530

    socket.io bug fix try 1

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,32 +5,32 @@ import express from "express";
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server,{
-    cors:{
-        origin:["http://localhost:3000"],
-        methods:["GET","POST"]
-    }
+const io = new Server(server, {
+  cors: {
+    origin: ["http://localhost:3000", "https://chatcourt.onrender.com"],
+    methods: ["GET", "POST"],
+  },
 });
 
 export const getReceiverSocketId = (receiverId) => {
-    return userSocketMap[receiverId];
+  return userSocketMap[receiverId];
 };
 
 const userSocketMap = {}; //{userId:socketId}
 
-io.on("connection",(socket) => {
-    console.log("User connected",socket.id);
+io.on("connection", (socket) => {
+  console.log("User connected", socket.id);
 
-    const userId = socket.handshake.query.userId;
-    if(userId!="undefined") userSocketMap[userId] = socket.id;
+  const userId = socket.handshake.query.userId;
+  if (userId != "undefined") userSocketMap[userId] = socket.id;
 
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    //socket.on is used to listen to events on both the server and the client
-    socket.on("disconnect",() => {
-        console.log("User disconnected", socket.id);
-        delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
-    });
+  io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  //socket.on is used to listen to events on both the server and the client
+  socket.on("disconnect", () => {
+    console.log("User disconnected", socket.id);
+    delete userSocketMap[userId];
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
+  });
 });
 
 export { app, io, server };
